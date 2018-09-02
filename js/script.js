aff905ecbc0834093b4c8a42739d854970ee75de
Emma Willmann <emma.anne.81@gmail.com>
2018-09-02 07:05:57 -0500

    added some search functionality - not fully working yet

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
 const pageDiv = document.querySelector('.page');
 const headerDiv = document.querySelector('.page-header');
 const searchDiv = createElement('div');
@@ -13,37 +12,106 @@ const studentUl = document.querySelector('ul');
 const studentLi = studentUl.children;
 
 
-
-
+//create Search field and button
 searchDiv.className = 'student-search';
 searchInput.type = 'text';
-searchInput.placeholder = "search for students..."
-searchButton.textContent = 'search';
+searchInput.placeholder = "Search for students..."
+searchButton.textContent = 'Search';
 
 headerDiv.appendChild(searchDiv);
 searchDiv.appendChild(searchInput);
 searchDiv.appendChild(searchButton);
 
 
+searchDiv.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+      const search = searchInput.value;
+      for ( let i = 0; i < studentLi.length; i +=1) {
+        const studentName = studentLi[i].querySelector('h3').textContent;
+        const holder = []
+        if (studentName.includes(search)) {
+          studentLi[i].style.display = 'list-item';
+        } else {
+          studentLi[i].style.display = 'none';
+        }
+      }
 
-//Create a function to hide all of the items in the list excpet for the ten you want to show
-//Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+    // function countFilter () {
+      let count = 0;
+      for ( let i = 0; i < studentLi.length; i +=1) {
+        if (studentLi[i].style.display === 'list-item') {
+          count += 1;
+        }
+        console.log(count);
+
+      //}
+      count = Math.ceil(count / 10);
+
+      for (let i = 0; i < pagLi.length; i += 1) {
+        pagLi[i].style.display = 'none';
+        if (i < count) {
+          pagLi[i].style.display = 'list-item';
+        } else {
+          pagLi[i].style.display = 'none';
+        }
+      }
+      pagLi[0].className = "active";
+
+    }
+
+  }
+});
+
+// searchDiv.addEventListener('keypress', (e) => {
+//    if (e.target.tagName === 'BUTTON') {
+//     const search = searchInput.value;
+//     for ( let i = 0; i < studentLi.length; i +=1) {
+//       const studentName = studentLi[i].querySelector('h3');
+//       console.log(studentName);
+//     }
+//   }
+// });
+
+// searchDiv.addEventListener('keyup', () => {
+//   const search = searchInput.value;
+//   console.log(search);
+// });
+
+
+
+
+//function to create elementes
 function createElement(element) {
   return document.createElement(element);
 }
 
-function createPag () {
+/*
+This function creates the pagination links on the page.
+It loops through the list of student names, adding 1 to the count each time.
+Divides that count by 10, and rounds up for total links needed
+Uses a loops, starting at 1 not 0, based on that count to create a LI and an A, setting class to active for the first A only.
+appends them together, then calls the setPag function, which hides list items so only 10 show.
+*/
+function count () {
   let count = 0;
-  for (let i = 0; i < studentLi.length; i +=1){
-    if (studentLi[i].style.display === "") {
+  for (let i = 0; i < studentLi.length; i += 1){
     count += 1;
-    }
   }
+  return count;
+}
+
+function createPag (count) {
+  // let count = 0;
+  // for (let i = 0; i < studentLi.length; i +=1){
+  //   //if (studentLi[i].style.display === "") {
+  //   count += 1;
+  //   //}
+  // }
+  //let count = count();
   count = Math.ceil(count / 10);
   if (count > 1) {
     const div = createElement('div');
     const ul = createElement('ul');
-    const li = createElement('li');
     div.className = 'pagination';
     for (i = 1; i <= count; i += 1) {
       const li = createElement('li');
@@ -61,14 +129,21 @@ function createPag () {
     setPag();
   }
 }
+//call the createPag function to set up the pagination for the first time
 
-createPag();
+createPag(count());
+//declared here, because these elements don't exist until after createPag is called.
 const pagDiv = document.querySelector('.pagination');
 const pagUl = pagDiv.children;
 const pagLi = pagUl[0].children;
 
-
-
+/*
+This function determines what student list items will show based on the active pagination link.
+uses a loop to see if the class of the link is active.
+if it is, it stores the text in that link as a number in active.
+creates a range of li to return.  example: if 2 is active, x 10 - 1 gives us 19 and -9 would give us 10, which is the range we need.
+for loop shows studentLi if it's index is between those 2 number, and hides all other items.
+*/
 
 function setPag () {
   const pagDiv = document.querySelector('.pagination');
@@ -80,7 +155,6 @@ function setPag () {
       const active = parseInt(pagLi[i].firstChild.textContent);
       const activeUpper = (active * 10) - 1;
       const activeLower = activeUpper - 9;
-      console.log(studentLi.length);
       for (let j = 0; j < studentLi.length; j += 1) {
         if (j >= activeLower && j <= activeUpper) {
           studentLi[j].style.display = 'list-item';
@@ -92,6 +166,11 @@ function setPag () {
   }
 }
 
+/*
+Listens for click on pagination links.
+loops through all pagination links and sets class to an empty string
+Then adds the active class to the clicked button
+*/
 pagUl[0].addEventListener('click', (e) => {
   if (e.target.tagName === 'A') {
     for (let i = 0; i < pagLi.length; i += 1) {
@@ -102,12 +181,3 @@ pagUl[0].addEventListener('click', (e) => {
     setPag();
   }
 });
-
-
-// Create and append the pagination links - Creating a function that can do this is a good approach
-
-
-
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
